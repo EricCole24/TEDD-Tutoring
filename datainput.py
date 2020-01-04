96127695be9d5595e99159509372769d39e76321
EricCole24 <colemaneric1994@gmail.com>
2020-01-04 05:44:31 -0500

    another

--- datainput.py
+++ datainput.py
@@ -1,34 +1,41 @@
 
-
-from flask import Flask, render_template,request,flash,session
+from datetime import timedelta
+from flask import Flask, render_template, request, flash, session,redirect,url_for
+from flask_login import LoginManager,login_user,UserMixin,logout_user, login_required,current_user
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect,CSRFError
 from flask_sqlalchemy import SQLAlchemy
-from OpenSSL import SSL
-from wtforms import Form,  validators, StringField,PasswordField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired,ValidationError,DataRequired
-from password import *
-from flask_login import logout_user,LoginManager,login_required,login_user
-from flask_migrate import Migrate, MigrateCommand
 from sqlalchemy.exc import *
+from wtforms import Form, validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
 import emailing
+from password import *
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 app =Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:0270091294@localhost/students'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 db = SQLAlchemy(app)
-
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+login_manager.login_view = "index"
 migrate = Migrate(app, db)
+user_datastore = SQLAlchemy
 
-#context =SSL.Context(SSL.TLSv1_2_METHOD)
-#context.use_certificate("mycert.crt")
-#context.use_privatekey("myprivate.key")
-
-
-class Data(db.Model):
+csrf = CSRFProtect(app)
+admin = Admin(app)
+sentry_sdk.init(
+    dsn="https://990d5ad8be404b839c7441234bae2fef@sentry.io/1871585",
+    integrations=[FlaskIntegration(),SqlalchemyIntegration()]
+)
+class Data(db.Model, UserMixin):
     __tablename__="studentsignup"
     id = db.Column(db.Integer, primary_key=True)
     Firstname = db.Column(db.String(120))
@@ -110,7 +117,8 @@ class Tutor(db.Model):
         self.Expertise = Expertise
         self.Reason = Reason
         self.Restriction = Restriction
-
+#US = SQLAlchemyUserDatastore(db, Data, StudentData)
+#security = Security(app,US)
 
 class ReusableForm(Form):
     firstname = StringField('Name:', validators=[DataRequired()])
@@ -132,6 +140,22 @@ class StudentForm(Form):
     comment = StringField('Email:', validators=[DataRequired()])
 
 
+
+admin.add_view(ModelView(Data,db.session))
+admin.add_view(ModelView(StudentData,db.session))
+
+@login_manager.user_loader
+def get_user(ident):
+  return Data.query.get(int(ident))
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.errorhandler(CSRFError)
+def handle_csrf_error(e):
+    return render_template('csrf_error.html', reason=e.description), 400
 
 @app.route("/")
 def index():
@@ -167,6 +191,8 @@ def login():
         except DataError:
             flash("Invalid data entry on contact. Enter 10 digits only")
             render_template("StudentReg.html")
+        except db.session.rollback():
+            raise
 
         #flash("all filed required")
         #flash("Password must match confirm password")
@@ -185,13 +211,16 @@ def signedin():
     if request.method == "POST":
         email = request.form["studentemail"]
         pword = request.form["studentpassword"]
+        remem = True if request.form.get("chk") else False
         print(pword)
         hashedpassword = db.session.query(Data.Password).filter(Data.Email == email).scalar()
-        user = db.session.query(Data.Email).filter(Data.Email == email).scalar()
+        user = Data.query.filter_by(Email = email).first()
+        print(user)
 
 
         if db.session.query(Data).filter(Data.Email == email).count() == 1 and check_password(hashedpassword,pword) == True:
-            session["log"] = True
+            #session["log"] = True
+            login_user(user,remember=remem)
             flash("login success")
             return render_template("mainStudent.html")
 
@@ -201,13 +230,12 @@ def signedin():
 
 
 
-
 @app.route("/logout")
-
+@login_required
 def logout():
-    session.clear()
+    logout_user()
     flash("loggout succesfully")
-    return render_template("welcome.html")
+    return redirect(url_for("index"))
 
 @app.route("/confirmation",methods=["POST"])
 def confirmation():
@@ -226,6 +254,29 @@ def confirmation():
         return render_template("confirmStudent.html")
     flash("all fields are required")
     return render_template("mainStudent.html")
+
+
+@app.route("/delete")
+@login_required
+def delete():
+    #if request.method == "POST":
+        #email = request.form["dname"]
+        #if db.session.query(Data).filter(Data.Email == email).count() == 1 and session["log"] == True:
+    if current_user.is_authenticated:
+        s = db.session.query(Data).filter(Data.Email == current_user.Email).delete()
+        db.session.commit()
+        print(s)
+
+        flash("We are sorry to see you go. You can always sign up with us again")
+        return render_template("welcome.html")
+    flash("email does not exist")
+    return render_template("ooo.html")
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
+
+
 
 if __name__ == '__main__':
     app.run(debug = True )
